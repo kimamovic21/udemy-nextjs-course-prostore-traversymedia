80d861a1c4844f514e5dcc6d63ff56a95390746c
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-14 17:35:46 +0100

    15. Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = async () => {
   await delay(1000);
 
   return (
-    <div>Homepage</div>
+    <>
+      <ProductList 
+        data={sampleData.products} 
+        title='Newest Arrivals' 
+        limit={4}
+      />
+    </>
   );
 };
 
