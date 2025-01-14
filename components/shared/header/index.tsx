83c9c52af1f536793d7b7019563ebc4777b41269
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-14 15:49:36 +0100

    14. Responsive Sheet Menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
 import Image from 'next/image';
 import Link from 'next/link';
-import ModeToggle from './mode-toggle';
+import Menu from './menu';
 
 const Header = () => {
   return (
@@ -24,23 +22,7 @@ const Header = () => {
           </Link>
         </div>
 
-        <div className='space-x-2'>  
-          <ModeToggle />
-
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
-              <span><ShoppingCart /></span>
-              <span>Cart</span>
-            </Link>
-          </Button>
-
-          <Button asChild>
-            <Link href='/sign-in'>
-              <span><UserIcon /></span>
-              <span>Sign In</span> 
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
