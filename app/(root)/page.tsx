b29c2b8d10a2bff99093eee3603e0db03cf0cec0
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-14 15:12:52 +0100

    13. Loading & Not Found Pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const Homepage = () => {
+const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
+const Homepage = async () => {
+  await delay(1000);
+
   return (
     <div>Homepage</div>
   );
