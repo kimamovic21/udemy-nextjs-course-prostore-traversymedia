a1abb6fecc8ad11c2bef7e7e15c85199671fb1f3
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-15 16:01:41 +0100

    22. Load Products From Database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 import ProductList from '@/components/shared/product/product-list';
 
 const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
@@ -6,10 +6,12 @@ const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout
 const Homepage = async () => {
   await delay(1000);
 
+  const latestProducts = await getLatestProducts();
+
   return (
     <>
       <ProductList 
-        data={sampleData.products} 
+        data={latestProducts} 
         title='Newest Arrivals' 
         limit={4}
       />
