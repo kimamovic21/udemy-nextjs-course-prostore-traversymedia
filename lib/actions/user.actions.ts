106dfaf85f43cddc88e936a4d33d4a9c9367341b
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-22 16:39:55 +0100

    40. Sign Up Zod Schema & Action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { hashSync } from 'bcrypt-ts-edge';
 import { signIn, signOut } from '@/auth';
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
+import { prisma } from '@/db/prisma';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -27,4 +29,44 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
 // Sign the user out
 export async function signOutUser() {
   await signOut();
+};
+
+// Sign up user 
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User registered successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    };
+
+    return {
+      success: false,
+      message: 'Something went wrong. Please try again.',
+    };
+  };
 };
