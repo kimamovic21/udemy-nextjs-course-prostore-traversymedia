2ff7750fd256675b6c16428dd861f27fdeb08d75
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-10 17:26:39 +0100

    47. Session Cart ID Cookie

--- auth.ts
+++ auth.ts
@@ -1,3 +1,5 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import type { NextAuthConfig } from 'next-auth';
 import { compareSync } from 'bcrypt-ts-edge';
@@ -93,6 +95,30 @@ export const config = {
       };
 
       return token;
+    },
+    authorized({ request, auth }): any {
+      // Check for session cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders
+          },
+        });
+
+        // Set newly generated sessionCartId in the response cookies
+        response.cookies.set('sessionCartId', sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      };
     },
   },
 } satisfies NextAuthConfig;
