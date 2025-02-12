e9f438284f7f440ea72bc63b2e5acaaa866dfc9a
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-12 23:03:04 +0100

    53. Smooth UI With useTransition Hook

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { useTransition } from 'react';
 import { useRouter } from 'next/navigation';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 import { type CartItem, type Cart } from '@/types';
 import { useToast } from '@/hooks/use-toast';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
@@ -12,41 +13,48 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
 
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        });
+
+        return;
+      };
+
+      // Handle success add to cart
       toast({
-        variant: 'destructive',
         description: res.message,
+        action: (
+          <ToastAction
+            className='bg-primary text-white hover:bg-gray-800'
+            altText='Go to cart'
+            onClick={() => router.push('/cart')}
+          >
+            Go to cart
+          </ToastAction>
+        ),
       });
-      return;
-    };
-
-    // Handle success add to cart
-    toast({
-      description: res.message,
-      action: (
-        <ToastAction
-          className='bg-primary text-white hover:bg-gray-800'
-          altText='Go to cart'
-          onClick={() => router.push('/cart')}
-        >
-          Go to cart
-        </ToastAction>
-      ),
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast({
-      variant: res.success ? 'default' : 'destructive',
-      description: res.message
+      toast({
+        variant: res.success ? 'default' : 'destructive',
+        description: res.message,
+      });
+
+      return;
     });
-
-    return;
   };
 
   // Check if item is in cart
@@ -55,18 +63,30 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Minus className='w-4 h-4' />
+        )}
       </Button>
-      <span className='px-2'>
-        {existItem.qty}
-      </span>
+      <span className='px-2'>{existItem.qty}</span>
       <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='w-4 h-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <span><Plus /></span>
+      <span>
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='w-4 h-4' />
+        )} 
+      </span>
       <span>Add To Cart</span>
     </Button>
   );
