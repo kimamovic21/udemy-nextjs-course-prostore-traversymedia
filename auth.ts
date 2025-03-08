465acac4d9f12338fa1ae25e16be31b0dae7e0cd
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-08 13:53:23 +0100

    62. Persist Session Cart

--- auth.ts
+++ auth.ts
@@ -88,6 +88,30 @@ export const config = {
             data: { name: token.name },
           });
         };
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId : user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            };
+          };
+        };
       };
 
       // Handle session updates (e.g., name change)
