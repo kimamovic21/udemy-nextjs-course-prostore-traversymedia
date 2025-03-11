eff07149e2a146f5d0f0cf26e951ff6150b2cc73
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-11 14:06:11 +0100

    70. Place Order Page

---
+++ app/(root)/place-order/page.tsx
@@ -0,0 +1,152 @@
+import { redirect } from 'next/navigation';
+import { type Metadata } from 'next';
+import { auth } from '@/auth';
+import { getMyCart } from '@/lib/actions/cart.actions';
+import { getUserById } from '@/lib/actions/user.actions';
+import { ShippingAddress } from '@/types';
+import { formatCurrency } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import Link from 'next/link';
+import Image from 'next/image';
+import CheckoutSteps from '@/components/shared/checkout-steps';
+
+export const metadata: Metadata = {
+  title: 'Place Order',
+};
+
+const PlaceOrderPage = async () => {
+  const cart = await getMyCart();
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error('User ID not found');
+
+  const user = await getUserById(userId);
+
+  if (!cart || cart.items.length === 0) redirect('/cart');
+  if (!user.address) redirect('/shipping-address');
+  if (!user.paymentMethod) redirect('/payment-method');
+
+  const userAddress = user.address as ShippingAddress;
+
+  return (
+    <>
+      <CheckoutSteps current={3} />
+
+      <h2 className='py-4 text-2xl'>Place Order</h2>
+
+      <div className='grid md:grid-cols-3 md:gap-5'>
+        <div className='overflow-x-auto md:col-span-2 space-y-4'>
+          <Card>
+            <CardContent className='p-4 gap-4'>
+              <h3 className='text-xl pb-4'>Shipping Address</h3>
+              <p>{userAddress.fullName}</p>
+              <p>
+                <span className='mr-1'>{userAddress.streetAddress},</span>
+                <span className='mr-1'>{userAddress.city},</span>
+                <span className='mr-1'>{userAddress.postalCode},</span>
+                <span>{userAddress.country}</span>
+              </p>
+              <div className='mt-3'>
+                <Link href='/shipping-address'>
+                  <Button variant='outline'>Edit</Button>
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardContent className='p-4 gap-4'>
+              <h3 className='text-xl pb-4'>Payment Method</h3>
+              <p>{user.paymentMethod}</p>
+              <div className='mt-3'>
+                <Link href='/payment-method'>
+                  <Button variant='outline'>Edit</Button>
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardContent className='p-4 gap-4'>
+              <h3 className='text-xl pb-4'>Order Items</h3>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Item</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead>Price</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {cart?.items?.map((item) => (
+                    <TableRow key={item.slug}>
+                      <TableCell>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className='flex items-center'
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                          <span className='px-2'>{item.name}</span>
+                        </Link>
+                      </TableCell>
+                      <TableCell>
+                        <span className='px-2'>{item.qty}</span>
+                      </TableCell>
+                      <TableCell className='text-right'>
+                        ${item.price}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Link href='/cart'>
+                <Button variant='outline'>Edit</Button>
+              </Link>
+            </CardContent>
+          </Card>
+        </div>
+
+        <div>
+          <Card>
+            <CardContent className='p-4 gap-4 space-y-4'>
+              <div className='flex justify-between'>
+                <div>Items</div>
+                <div>{formatCurrency(cart.itemsPrice)}</div>
+              </div>
+              <div className='flex justify-between'>
+                <div>Tax</div>
+                <div>{formatCurrency(cart.taxPrice)}</div>
+              </div>
+              <div className='flex justify-between'>
+                <div>Shipping</div>
+                <div>{formatCurrency(cart.shippingPrice)}</div>
+              </div>
+              <div className='flex justify-between'>
+                <div>Total</div>
+                <div>{formatCurrency(cart.totalPrice)}</div>
+              </div>
+              {/* Form Here */}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlaceOrderPage;
