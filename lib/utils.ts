32b5ed90e15283a66b016cfed24d42005ca9bb55
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-12 13:58:24 +0100

    74. Format Utility Functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,7 +39,7 @@ export function formatError(error: any): string {
 };
 
 // Round to 2 decimal places
-export const round2 = (value: number | string) => {
+export function round2(value: number | string) {
   if (typeof value === 'number') {
     return Math.round((value + Number.EPSILON) * 100) / 100; // avoid rounding errors
   } else if (typeof value === 'string') {
@@ -63,5 +63,51 @@ export function formatCurrency(amount: number | string | null) {
     return CURRENCY_FORMATTER.format(Number(amount));
   } else {
     return 'NaN';
+  };
+};
+
+// Shorten ID
+export function formatId(id: string) {
+  return `..${id.substring(id.length - 6)}`;
+};
+
+// Format date and time
+export function formatDateTime(dateString: Date) {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    month: 'short', // abbreviated month name (e.g., 'Oct')
+    year: 'numeric', // abbreviated month name (e.g., 'Oct')
+    day: 'numeric', // numeric day of the month (e.g., '25')
+    hour: 'numeric', // numeric hour (e.g., '8')
+    minute: 'numeric', // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short', // abbreviated weekday name (e.g., 'Mon')
+    month: 'short', // abbreviated month name (e.g., 'Oct')
+    year: 'numeric', // numeric year (e.g., '2023')
+    day: 'numeric', // numeric day of the month (e.g., '25')
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric', // numeric hour (e.g., '8')
+    minute: 'numeric', // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+  };
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateTimeOptions
+  );
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    'en-US',
+    dateOptions
+  );
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    'en-US',
+    timeOptions
+  );
+
+  return {
+    dateTime: formattedDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
   };
 };
