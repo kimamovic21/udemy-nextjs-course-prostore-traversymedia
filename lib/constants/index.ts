ce57aa992e7a39d7b61ec32faf09f7cb6d09b0e8
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-15 14:39:50 +0100

    87. Get My Orders Action

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,9 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern store built with Next.js';
+
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
@@ -28,3 +31,5 @@ export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   : ['PayPal', 'Stripe', 'CashOnDelivery'];
   
 export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
--- types/index.ts
+++ types/index.ts
@@ -16,9 +16,13 @@ export type Product = z.infer<typeof insertProductSchema> & {
 };
 
 export type Cart = z.infer<typeof insertCartSchema>;
+
 export type CartItem = z.infer<typeof cartItemSchema>;
+
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
 export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+
 export type Order = z.infer<typeof insertOrderSchema> & {
   id: string;
   createdAt: Date;
@@ -29,4 +33,5 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   orderItems: OrderItem[];
   user: { name: string; email: string };
 };
+
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
