828cda6c84374764b34cff43ccae109d67ab90a0
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-17 15:48:16 +0100

    91. Update Profile Form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,6 +1,22 @@
-const ProfilePage = () => {
+import { SessionProvider } from 'next-auth/react'
+import { type Metadata } from 'next';
+import { auth } from '@/auth';
+import ProfileForm from './profile-form';
+
+export const metadata: Metadata = {
+  title: 'Customer Profile'
+};
+
+const ProfilePage = async () => {
+  const session = await auth();
+
   return (
-    <div>Profile Page</div>
+    <SessionProvider session={session}>
+      <div className='max-w-md mx-auto space-y-4'>
+        <h2 className='h2-bold'>Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
   );
 };
 
