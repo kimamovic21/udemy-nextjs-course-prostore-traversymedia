64e5165da209de34d84d100cd937f04c50fea21f
kimamovic21 <imamovic.kerim@gmail.com>
2025-03-21 14:27:24 +0100

    101. Update Order Actions (Cash On Delivery)

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -393,3 +393,55 @@ export async function deleteOrder(id: string) {
     };
   };
 };
+
+// Update Cash on Delivery order to paid
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    await updateOrderToPaid({ orderId });
+    revalidatePath(`/order/${orderId}`);
+
+    return { 
+      success: true, 
+      message: 'Order marked as paid' 
+    };
+  } catch (err) {
+    return { 
+      success: false, 
+      message: formatError(err) 
+    };
+  };
+};
+
+// Update Cash on Delivery order to delivered
+export async function deliverOrder(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (!order) throw new Error('Order not found');
+    if (!order.isPaid) throw new Error('Order is not paid');
+
+    await prisma.order.update({
+      where: { id: orderId },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
+
+    revalidatePath(`/order/${orderId}`);
+
+    return { 
+      success: true, 
+      message: 'Order marked as delivered' 
+    };
+  } catch (err) {
+    return { 
+      success: false, 
+      message: formatError(err) 
+    };
+  };
+};
