631eef12b26e4055ab10a77ce3559e80659468c1
Kerim Imamović <imamovic.kerim@gmail.com>
2025-04-04 15:39:57 +0200

    107. Create and Update Actions

--- lib/validators.ts
+++ lib/validators.ts
@@ -23,6 +23,11 @@ export const insertProductSchema = z.object({
   price: currency,
 });
 
+// Schema for updating a product
+export const updateProductSchema = insertProductSchema.extend({
+  id: z.string().min(1, 'Id is required'),
+});
+
 // Schema for signing in a user
 export const signInFormSchema = z.object({
   email: z.string().email('Invalid email address').min(6, 'Email must be at least 6 characters'),
