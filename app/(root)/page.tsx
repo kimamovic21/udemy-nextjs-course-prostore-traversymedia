432d7bcef19ab44d281a4ea2eb6f678924f35774
Kerim Imamović <imamovic.kerim@gmail.com>
2025-04-11 17:50:43 +0200

    127. Featured Products Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,23 @@
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import { 
+  getLatestProducts, 
+  getFeaturedProducts 
+} from '@/lib/actions/product.actions';
 import ProductList from '@/components/shared/product/product-list';
+import ProductCarousel from '@/components/shared/product/product-carousel';
 
 const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = async () => {
-  await delay(1000);
+  await delay(500);
 
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList 
         data={latestProducts} 
         title='Newest Arrivals' 
